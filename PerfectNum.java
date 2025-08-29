9e0123ef9d953e5845bc0c800c21a35a35538387
Pardeshi Rahul <rahulpardeshi949@gmail.com>
2025-08-29 13:14:54 +0530

    Add files via upload

---
+++ PerfectNum.java
@@ -0,0 +1,24 @@
+package preac.pac;
+
+public class PerfectNum {
+
+	public static void main(String[] args) {
+		int n=100;
+		for(int i=1;i<n;i++)
+		{
+		
+		int sum=0;
+		for(int j=1 ; j<=i/2;j++)
+		{
+			if(i% j==0)
+			{
+				sum+=j;
+			}
+		}
+		if(sum==i)
+		{
+			System.out.println("perfect number"+i);
+		}
+		}
+	}
+}
